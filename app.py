828e3a052f82a37403e94601faf3f485f802a043
khanin2539 <32312973+khanin2539@users.noreply.github.com>
2021-02-13 22:46:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 from absl import app, flags, logging
 from absl.flags import FLAGS
 import core.utils as utils
