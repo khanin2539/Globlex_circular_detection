21767c612636a973e9c00803ea931a8ff7c660b9
khanin2539 <32312973+khanin2539@users.noreply.github.com>
2021-02-13 22:33:22 +0700

    Update detect1.py

--- detect1.py
+++ detect1.py
@@ -2,7 +2,6 @@ import os
 # comment out below line to enable tensorflow outputs
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
-from keras.preprocessing.image import load_img
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
